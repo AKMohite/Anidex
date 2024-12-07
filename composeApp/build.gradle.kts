914698c12e959fa0cf2e209777a4d8570fee613f
Ashish Mohite <ashishmohite1996@gmail.com>
2024-12-07 12:32:02 +0530

    Add coil

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -74,6 +74,8 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.coil.compose)
+            implementation(libs.coil.network.ktor)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(libs.koin.core)
